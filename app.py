0a97e4d4c6da1065c79ae9b270e17b0541f14a94
suraj646 <suraj64u@gmail.com>
2025-04-01 23:30:35 +0530

    This is the commit web application

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from django.shortcuts import render
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+ss=StandardScaler()
+
+app = Flask(__name__, template_folder='template')
+
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict_api',methods=['Post'])
+def predictr_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    ss.fit(np.array(list(data.values())).reshape(1,-1))
+    new_data=ss.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__=="__main__":
+    app.run(debug=True)
